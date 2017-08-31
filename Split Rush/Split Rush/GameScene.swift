a37f978b4030308d42e3c591c321e44ebb98bcd8
DMeechan <daniel.meechan.1+git@gmail.com>
2017-08-31 13:45:26 +0100

    Game works

--- Split Rush/Split Rush/GameScene.swift
+++ Split Rush/Split Rush/GameScene.swift
@@ -18,13 +18,83 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
     physicsWorld.contactDelegate = self
     addPlayers()
-    addRow(type: RowType.twoS)
+    addRow(type: RowType.oneL)
+    
+  }
+  
+  func addRandomRow() {
+    let randomNumber = Int(arc4random_uniform(6))
+    
+    guard let rowType = RowType(rawValue: randomNumber) else { return }
+    
+    addRow(type: rowType)
+    
+  }
+  
+  var lastUpdateTimeInterval = TimeInterval()
+  var lastYieldTimeInterval = TimeInterval()
+  
+  func updateWithTimeSinceLastUpdate(timeSinceLastUpdate: CFTimeInterval) {
+    lastYieldTimeInterval += timeSinceLastUpdate
+    
+    if lastYieldTimeInterval > 0.6 {
+      lastYieldTimeInterval = 0
+      addRandomRow()
+    }
     
   }
   
   
   override func update(_ currentTime: TimeInterval) {
     // Called before each frame is rendered
+    
+    var timeSinceLastUpdate = currentTime - lastUpdateTimeInterval
+    lastUpdateTimeInterval = currentTime
+    
+    if timeSinceLastUpdate > 1 {
+      timeSinceLastUpdate = 1 / 60
+      lastUpdateTimeInterval = currentTime
+      
+    }
+    
+    updateWithTimeSinceLastUpdate(timeSinceLastUpdate: timeSinceLastUpdate)
+    
   }
+
+  
+  func didBegin(_ contact: SKPhysicsContact) {
+    // BodyA = obstacle; BodyB = player
+    if contact.bodyB.node?.name == "PLAYER" {
+      // Show game over scene
+      print("GAME OVER")
+    }
+  }
+  
+  // MARK: Player movement
+  
+  func resetPlayerPosition() {
+    players[0].position = initialPlayerPos
+    players[1].position = initialPlayerPos
+    
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    resetPlayerPosition()
+  }
+  
+  // MARK: 3D-TOUCH NEEDED:
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if let touch = touches.first {
+      let maxPossibleForce = touch.maximumPossibleForce
+      let force = touch.force
+      let normalizedForce = force / maxPossibleForce
+      
+      players[0].position.x = (self.size.width / 2) - normalizedForce * (self.size.width / 2 - 25)
+      players[1].position.x = (self.size.width / 2) + normalizedForce * (self.size.width / 2 - 25)
+      
+    }
+  }
+  
   
 }
